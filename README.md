4ae3341cf2e1bb87df8aaadc18d5454fe52d36ba
kevinbeirne1 <kevingbeirne@gmail.com>
2021-10-27 17:20:38 -0400

    Added requirement.txt, updated Readme with more detailed outline, added docstrings to __main__.py

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # Crossword GUI
+The Crossword GUI is a python script to gather the latest crosswords from the New Yorker into a single app. 
 
-The Crossword GUI is an app to scrape the New Yorker website for links to their crosswords and display them in the GUI. Intention for future revisions is among other things to enable offline solving of the crosswords
+CONTENTS
+========
+* [Why](#why)
+* [Setup](#setup)
+* [How to run](#how-to-run)
+* [How it works](#how-it-works)
+* [Things to work on](#things-to-work-on)
+
+## Why
+### Background
+Over the course of the Covid induced lockdown I became increasingly interested in solving crosswords and almost exclusively the New York Times (NYT) crossword.
+During this time of increased interest in crosswords I became aware of some very public and damning criticisms of the NYT crossword (including the [Atlantic](https://www.theatlantic.com/culture/archive/2020/03/fight-to-make-crosswords-more-inclusive/608212/) and [Time](https://time.com/5871704/erik-agard-usatoday-crossword-diversity/) magazines).
+I then started searching for other outlets and was drawn to the New Yorker puzzles because of their (subjectively) great puzzle creators.
+
+### No New Yorker Crossword App
+I feel a big reason that I got into solving the NYT crossword was because their mobile app made it very easy to access/complete.
+Unfortunately at the present time the New Yorker does not have an android app (for either the magazine or crossword). 
+My goal is to eventually create a standalone mobile app to solve the New Yorker crosswords on android.
+Creating a desktop application in python seemed a reasonable first step, with porting being a future step.
+
+
+## Setup
+NOTE: This project used the excellent [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/) for creating and managing the virtual environment. 
+virtualenvwrapper is not necessary to run to this project, simply replace `mkvirtualenv` and `workon` below with your chosen way of creating/activating a virtual environment.  
+
+- Clone the repo 
+  `$ git clone https://github.com/kevinbeirne1/Crossword-GUI-App.git`
+- Create/activate the virtualenv
+  `$ mkvirtualenv -a <project_directory> <virtualenv_name>`
+- Install the project requirements
+  `$ pip install -r requirements.txt`
+
+## How to run
+- Activate the virtual environment
+  `$ workon <virtualenv_name`
+- Run the program
+  `$ python crossword_gui/__main__.py`
+- Answer yes to search for the latest crosswords
+- In the left panel click the crossword you want to do
+- Work on the crossword in the right panel
+
+## How it works
+The project makes use of the [scrapy](https://docs.scrapy.org/en/latest/) and [PyQt5](https://www.riverbankcomputing.com/static/Docs/PyQt5/) libraries
+
+- On starting the program the user is asked whether to search for the latest crosswords
+- If yes: 
+  - The spider crawls through the New Yorker crosswords puzzles page to get 
+direct links to the crosswords
+  - The spider appends any new scraped data to `crossword_data.json`
+- If no:
+  - The spider doesn't run and proceeds to GUI launch
+
+
+- The GUI launches displaying two panes
+  - The left has a list of crosswords from `crossword_data.json` 
+  - The right has an embedded browser for displaying the crossword
+- Clicking on a crossword in the left pane loads the corresponding crossword in the
+browser window
+
+## Things to work on
+
+### GUI
+  - Add tick box to left section (beside each crossword name) to mark completed
+  - Make left/right section divider adjustable
+  - Add maximize/minimize buttons to control pane
+
+### Other Crossword Formats
+  - Generate algorithm to parse .puz files to get clues, answers, grid shape
+  - Create a GUI to translate the parsed .puz data to a grid
+  - Enable Add functionality to GUI/program to add local files to the library
+
+### Offline Solving
+  - Update the scrapy spider to save the new yorker crosswords to a local directory
+
+### Android
+  - Generate an APK for the app
+  - Run on a VM to check operation
+  - Some potential issues that may arise on mobile:
+    - Size of libraries required to run: PyQt library is ~350mb on PC
+    - Existing New Yorker crossword format not optimised for mobile
+    - Sectioned window not suitable for mobile
+
+
+
+
+
+
+
--- crossword_gui/__main__.py
+++ crossword_gui/__main__.py
@@ -1,5 +1,4 @@
-#! python3
-
+#! /home/kevin/.virtualenvs/crossword_gui/bin python
 """
 new_yorker_crossword_getter.py - GUI to pull all New Yorker Crosswords
 
@@ -19,7 +18,6 @@ import pyinputplus as pyip
 from collections import OrderedDict
 from datetime import datetime
 from operator import itemgetter
-from pathlib import Path
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import (QApplication, QDialog, QGroupBox, QVBoxLayout,
@@ -30,14 +28,18 @@ from scrapy.exceptions import DropItem
 
 
 class FormatItemPipeline:
-    """Format spider item data for readability"""
+    """Pipeline to format scraped spider item data to desired style"""
     def process_item(self, item, spider):
+        """
+        Attempt to format the scraped item. If error raised, drop the item
+        """
         try:
-            item["name"] = re.match(r".+\:\s?(.*)", item["name"]).group(1)
+            item["name"] = re.match(r".+:\s?(.*)", item["name"]).group(1)
             item["date"] = datetime.strptime(item['date'], "%B %d, %Y")
             item['url'] = re.match("(.+weekly)", item['url']).group(1)
-        except:
-            DropItem("Link is article, Not crossword")
+        except (AttributeError, ValueError, TypeError) as ItemError:
+            raise DropItem(f"{ItemError} - Link is article, Not crossword")
+
         return item
 
 
@@ -48,6 +50,7 @@ class SaveItemPipeline:
 
 
 class MyItem(Item):
+    """Specify Item fields that the spider is going to collect"""
     name = Field()
     date = Field()
     url = Field()
@@ -55,16 +58,18 @@ class MyItem(Item):
 
 
 class SpiderManager:
+    """Class to manage and run the scrapy spider"""
     spider_data = []
+
     def __init__(self):
+        """Run the spider, add scraped data a json file, read json for crossword links"""
         self.run_spider()
         self.update_json()
         self.crossword_data = self.read_json()
-        self.last_updated = self.crossword_data.pop("last_updated")
 
     @staticmethod
     def write_json(data, filename="crossword_data.json"):
-        """Write data to JSON file"""
+        """Write data to a JSON file"""
         with open(filename, "w") as f:
             json.dump(data, f, indent=4)
 
@@ -82,7 +87,12 @@ class SpiderManager:
 
     @staticmethod
     def update_json(data_file="crossword_data.json"):
-        """Read spider.json, update data.json, and delete spider.json"""
+        """
+        Sort the scraped spider data by the date field.
+        Read the existing crossword data from crossword_data.json
+        Add the scraped data to the crossword data.
+        Write the updated crossword data to crossword_data.json
+        """
         sorted_spider = sorted(SpiderManager.spider_data,
                                key=itemgetter("date"))
         crossword_data = OrderedDict(SpiderManager.read_json(data_file))
@@ -91,25 +101,19 @@ class SpiderManager:
             name = f'{crossword["name"]} - {crossword["author"]}'
             crossword_data.update({name: crossword["url"]})
             crossword_data.move_to_end(name, last=False)
-        # if sorted_spider:
-        crossword_data.update(
-            {"last_updated": str(sorted_spider[0]["date"])}
-        )
-        # else:
-        #     crossword_data["last_updated"] = None
         SpiderManager.write_json(crossword_data)
 
-    def check_run(func):
+    def check_run(self):
         """Ask user if they wish to run the spider & run if yes"""
         def wrapper(*args, **kwargs):
-            if pyip.inputYesNo("Do you want to run the spider (yes/no)?") \
+            if pyip.inputYesNo("Do you want to fetch the latest puzzles (yes/no)?") \
                     == "yes":
-                func(*args, **kwargs)
+                self(*args, **kwargs)
         return wrapper
 
     @check_run
     def run_spider(self):
-        """Run Spider if Mon/Wed/Fri or if no links saved"""
+        """Run NewYorkerSpider"""
 
         process = CrawlerProcess({
             "ITEM_PIPELINES": {
@@ -122,10 +126,14 @@ class SpiderManager:
 
 
 class NewYorkerSpider(scrapy.Spider):
+    """
+    Spider to scrape the direct crossword urls from the New Yorker puzzles page
+    """
     name = "new_yorker_crossword"
     temp_data = {}
 
     def start_requests(self):
+        """Specify the URLs to crawl through"""
         urls = [
             'https://www.newyorker.com/puzzles-and-games-dept/crossword',
         ]
@@ -133,29 +141,41 @@ class NewYorkerSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        previously_scraped = SpiderManager.read_json()
+        """
+        Parse the crossword puzzles main page to get the following for each puzzle:
+        - URL
+        - Crossword Title
+        - Crossword Author
+        - Publish Date
+
+        Then follow each URL and scrape with `parse_crossword`
+        """
 
         crossword_links = response.css("li.River__riverItem___3huWr")
         for crossword_link in crossword_links:
             item = MyItem()
-            url = crossword_link.css("a::attr(href)").get()
+            main_page_url = crossword_link.xpath("div/a/@href").get()
             item['name'] = crossword_link.css("h4::text").get()
             item['author'] = crossword_link.css("p a::text").get()
-            item['date'] = crossword_link.css("h6::text").get()
+            item['date'] = crossword_link.xpath(".//h6/text()").get()
 
-            # if url in previously_scraped.values():
-            #     break
-            yield response.follow(url, self.parse_crossword,
+            yield response.follow(main_page_url, self.parse_crossword,
                                   cb_kwargs=dict(item=item,)
                                   )
 
     def parse_crossword(self, response, item):
+        """
+        Parse the url from the main page to get the url to the crossword host site
+        """
         item['url'] = response.xpath(
             "//iframe[@id='crossword']/@data-src").get()
+        # item['date'] = response.xpath(
+        #     ".//time[@data-testid='ContentHeaderPublishDate']/text()").get()
         yield item
 
 
 def window():
+    """Start the PyQt window"""
     app = QApplication([])
     win = WidgetGallery()
     win.showMaximized()
@@ -165,9 +185,13 @@ def window():
 class WidgetGallery(QDialog):
 
     def __init__(self, parent=None):
+        """
+        Get the crossword from SpiderManager and start the GUI
+        """
         super(WidgetGallery, self).__init__(parent)
-        self.embedded_browser = self.create_embedded_browser()
         self.button_links = SpiderManager().crossword_data
+
+        self.embedded_browser = self.create_embedded_browser()
 
         self.create_left_group_box()
         self.create_right_group_box()
@@ -177,41 +201,54 @@ class WidgetGallery(QDialog):
         main_layout.addWidget(self.right_group_box)
         self.setLayout(main_layout)
 
-    def change_site(self, url):
-        self.embedded_browser.setUrl(QUrl(url))
-
-    def clicked(self):
-        item = self.crossword_list.currentItem()
-        self.change_site(self.button_links[item.text()])
-
     def create_left_group_box(self):
+        """
+        Left box group contains:
+         - list of crosswords
+         - Clicking item in the list loads it's url with self.clicked
+        """
         self.left_group_box = QGroupBox("Crossword list")
         self.left_group_box.setMaximumWidth(400)
 
         self.crossword_list = QListWidget()
         self.crossword_list.addItems(self.button_links)
         self.crossword_list.clicked.connect(self.clicked)
-        # self.crossword_list.setCurrentIndex()
 
         layout = QVBoxLayout()
         layout.addWidget(self.crossword_list)
         layout.setContentsMargins(10, 10, 10, 10)
         self.left_group_box.setLayout(layout)
 
-    def create_embedded_browser(self):
-        _embedded_browser = QWebEngineView()
-        _embedded_browser.setUrl(QUrl("http://google.com"))
-        return _embedded_browser
-
     def create_right_group_box(self):
+        """
+        Create right box group with the embedded browser
+        """
         self.right_group_box = QGroupBox("Browser")
 
         layout = QVBoxLayout()
         layout.addWidget(self.embedded_browser)
         self.right_group_box.setLayout(layout)
 
+    def change_site(self, url):
+        """Change the embedded browser url to the provided url"""
+        self.embedded_browser.setUrl(QUrl(url))
+
+    def clicked(self):
+        """
+        Get the url from the crossword_data dictionary and change
+        the embedded browser to this url
+        """
+        item = self.crossword_list.currentItem()
+        self.change_site(self.button_links[item.text()])
+
+    @staticmethod
+    def create_embedded_browser(self):
+        """Create an embedded browser for the GUI """
+        _embedded_browser = QWebEngineView()
+        _embedded_browser.setUrl(QUrl("http://google.com"))
+        return _embedded_browser
+
 
 if __name__ == '__main__':
-    # SpiderManager()
     window()
-    # help(QListWidget.setCurrentIndex)
+
